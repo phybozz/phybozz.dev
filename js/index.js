ac294093adce55e3c663aec46c0623e05c8e47d8
phybozz <alexanderbeast2.0@gmail.com>
2026-03-15 02:44:02 +0100

    massive, MASSIVE scaling update

--- js/index.js
+++ js/index.js
@@ -58,7 +58,7 @@ function handleParallaxMovement(e) {
     const moveY = y * mouseSpeedFactor * 25;
 
     happywrap.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    welcome.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
+    welcome.style.transform = `translate(${moveX}px, ${moveY}px)`;
 }
 
 window.addEventListener('mousemove', handleParallaxMovement);
